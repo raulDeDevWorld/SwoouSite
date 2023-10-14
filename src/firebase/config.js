9f5c56e8d05153644cebc4f5a023214f82d542b7
raulDeDevWorld <proyection009@gmail.com>
2023-10-14 19:37:37 -0400

    DC

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDe4P1_oZH8mq35LXj7xQpb9TdLHiAeglI",
-  authDomain: "autorepuestos-2780e.firebaseapp.com",
-  databaseURL: "https://autorepuestos-2780e-default-rtdb.firebaseio.com",
-  projectId: "autorepuestos-2780e",
-  storageBucket: "autorepuestos-2780e.appspot.com",
-  messagingSenderId: "451547279920",
-  appId: "1:451547279920:web:0aedeb14b3112de5a373f5"
-}
+  apiKey: "AIzaSyDr9Q97NIjzcaQdhQWoSGKH1SFaNcvnYSw",
+  authDomain: "lanfvet.firebaseapp.com",
+  databaseURL: "https://lanfvet-default-rtdb.firebaseio.com",
+  projectId: "lanfvet",
+  storageBucket: "lanfvet.appspot.com",
+  messagingSenderId: "771853011208",
+  appId: "1:771853011208:web:50da89bae1662ed2d144bd"
+};
 
 export const app = initializeApp(firebaseConfig);
